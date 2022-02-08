fe83b549979ab4fb2b04ef263b5471730d38f1d5
Kiyong Kim <scgyong@gmail.com>
2022-02-08 11:26:54 +0900

    Closure 까지

--- Day2/Day2.playground/Contents.swift
+++ Day2/Day2.playground/Contents.swift
@@ -9,6 +9,11 @@ func points(num:Int, bonus:(Int)->Bool) -> Int {
     return pt
 }
 
+let v = points(num: 4) {
+    $0 % 2 != 0
+}
+v
+
 func odd(nnn:Int) -> Bool {
     return (nnn % 2) != 0
 }
@@ -20,6 +25,58 @@ func neg(nn:Int) ->Bool {
     return nn < 0
 }
 
+let negative = { (n:Int)->Bool in
+    n < 0
+}
+
+negative(10)
+negative(-10)
+
 points(num: 7, bonus: neg)
 points(num: -5, bonus: neg)
+
+func incrementor(num:Int) -> (()->Int) {
+    var value = 0
+    func increase() -> Int {
+        value += num
+        return value
+    }
+    return increase
+}
+
+let inc3 = incrementor(num: 3)
+let inc7 = incrementor(num: 7)
+inc3()
+inc3()
+inc7()
+inc7()
+inc3()
+
+func closure_only(bonus:(Int)->Bool) -> Int {
+    let num = 5
+    var pt = num * 10
+    if bonus(num) {
+        pt += 1
+    }
+    return pt
+}
+
+closure_only {
+    $0 > 0
+}
+
+var nums = [ 1,43,43,54,23,65,23,543,32,54,43]
+nums.sort(by: {(n1:Int, n2:Int)->Bool in n1 < n2 })
+nums
+nums.sort(by: {n1, n2 in n1 > n2 })
+nums
+nums.sort(by: {$0 < $1})
+nums
+nums.sort {$0 > $1}
+nums
+nums.sort(by:<)
+nums
+nums.sort()
+
+//let gt:((Double,Double)->Bool) = >
 
