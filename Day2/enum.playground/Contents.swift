13d7847511443808e438d7dc066c7c2a2915191e
Kiyong Kim <scgyong@gmail.com>
2022-02-09 11:26:52 +0900

    FlagsSample skeleton

--- Day2/enum.playground/Contents.swift
+++ Day2/enum.playground/Contents.swift
@@ -41,3 +41,48 @@ func describe(about sr: ServerResponse) -> String {
 
 describe(about: resp)
 describe(about: err)
+
+protocol Increasing {
+    func increasedValue(amount:Int) -> Int
+    mutating func increase(amount: Int)
+}
+
+extension Int: Increasing {
+    func increasedValue(amount: Int) -> Int {
+        return self + amount
+    }
+    mutating func increase(amount: Int) {
+        self += amount
+    }
+}
+
+var v = 3
+v.increase(amount: 10)
+
+3.increasedValue(amount: 4)
+//3.increase(amount: 4)
+
+var aaa = [ 6, 1, 2 ]
+aaa.append(3)
+
+aaa.sorted()
+aaa
+aaa.sort()
+aaa
+
+extension Double: Increasing {
+    func increasedValue(amount: Int) -> Int {
+        return Int(self) + amount
+    }
+    mutating func increase(amount: Int) {
+        self += Double(amount)
+    }
+}
+
+10.increasedValue(amount: 3)
+20.7.increasedValue(amount: 4)
+
+let numbers:[Increasing] = [ 5, 6.7 ]
+for n in numbers {
+    n.increasedValue(amount: 500)
+}
---
+++ Day3/FlagsSample/FlagsSample/FlagsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsSampleApp.swift
+//  FlagsSample
+//
+//  Created by Kiyong Kim on 2022/02/09.
+//
+
+import SwiftUI
+
+@main
+struct FlagsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
