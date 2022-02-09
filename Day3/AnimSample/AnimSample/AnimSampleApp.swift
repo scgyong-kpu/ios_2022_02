225e6901bdacb630d18514fd515ffbeff8615cdc
Kiyong Kim <scgyong@gmail.com>
2022-02-09 16:08:11 +0900

    AnimSample skeleton

---
+++ Day3/AnimSample/AnimSample/AnimSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimSampleApp.swift
+//  AnimSample
+//
+//  Created by Kiyong Kim on 2022/02/09.
+//
+
+import SwiftUI
+
+@main
+struct AnimSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
