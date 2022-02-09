6c3b4e12374909eea70f542501e222989c63830e
Kiyong Kim <scgyong@gmail.com>
2022-02-09 16:28:09 +0900

    Rectangle, RoundedRectangle, Circle, Path, Capsule, Gradiant, Dash

--- Day3/AnimSample/AnimSample/ContentView.swift
+++ Day3/AnimSample/AnimSample/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Path { path in
+                path.move(to: CGPoint(x: 10, y: 0))
+                path.addLine(to: CGPoint(x: 10, y: 35))
+                path.addLine(to: CGPoint(x: 30, y: 30))
+                path.closeSubpath()
+            }
+            .fill(Color.green)
+            .frame(width: 200, height: 100)
+            Circle()
+                .fill(
+                    RadialGradient(gradient: Gradient(colors:[
+                        Color.red, Color.yellow, Color.green, Color.blue, Color.purple
+                    ]), center: .center, startRadius: 0, endRadius: 300)
+                )
+            .foregroundColor(Color.red)
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(lineWidth:8)
+                .frame(width:240, height:120)
+            Capsule()
+                .stroke(lineWidth: 5)
+                .foregroundColor(Color.blue)
+                .frame(width: 200, height: 60)
+        }
     }
 }
 
