ef34e6c3509c8e90fc0cb88dcbc4cae26bb24228
Kiyong Kim <scgyong@gmail.com>
2022-02-10 10:45:52 +0900

    MemoryGame skeleton

---
+++ Day4/MemoryGame/MemoryGame/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  MemoryGame
+//
+//  Created by Kiyong Kim on 2022/02/10.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ Day4/MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Kiyong Kim on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
