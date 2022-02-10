421eef4c0f90083450bbd0ba2f7999436b3a5a4e
Kiyong Kim <scgyong@gmail.com>
2022-02-10 16:33:23 +0900

    AppIcon 추가.

--- Day4/MemoryGame/MemoryGame/ContentView.swift
+++ Day4/MemoryGame/MemoryGame/ContentView.swift
@@ -21,5 +21,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
+.previewInterfaceOrientation(.portrait)
     }
 }
