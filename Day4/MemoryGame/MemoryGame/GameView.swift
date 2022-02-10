1eb4ab36dea02f0d466d3ee056e4c6396e554827
Kiyong Kim <scgyong@gmail.com>
2022-02-10 17:09:13 +0900

    Menu 꾸미기

--- Day4/MemoryGame/MemoryGame/GameView.swift
+++ Day4/MemoryGame/MemoryGame/GameView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct GameView: View {
+    let prefix: String
     @ObservedObject var game = GameModel()
     @Environment(\.presentationMode) var present
     @State var showsRestartAlert = false
@@ -21,7 +22,7 @@ struct GameView: View {
             }
             .padding(.horizontal)
             GridStack(rows: GameModel.rows, columns: GameModel.cols) { row, col in
-                CardView(prefix: "f", card: game.card(row: row, col: col))
+                CardView(prefix: prefix, card: game.card(row: row, col: col))
                     .gesture(TapGesture().onEnded {
                         game.toggle(row: row, col: col)
                         let card = game.card(row: row, col: col)
@@ -88,6 +89,6 @@ struct GameView: View {
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        GameView()
+        GameView(prefix: "f")
     }
 }
---
+++ Day4/MemoryGame/MemoryGame/MenuView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by Kiyong Kim on 2022/02/10.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    let resourcePrefixes = [ "f", "t" ]
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: 10) {
+                        ForEach (resourcePrefixes, id:\.self) { prefix in
+                            MenuItemView(prefix: prefix)
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [.white, .orange.opacity(0.5)]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+            )
+            .navigationBarTitle(Text("Memory Game"))
+        }
+    }
+}
+struct MenuItemView: View {
+    let prefix: String
+    var body: some View {
+        HStack {
+            Image("\(prefix)_back")
+                .resizable()
+                .frame(
+                    width: UIScreen.main.bounds.width * 0.4,
+                    height: UIScreen.main.bounds.width * 0.4)
+            Spacer()
+            NavigationLink(destination: GameView(prefix:prefix)) {
+                Text("Play Game")
+                    .padding()
+                    .background(
+                        Capsule()
+                            .stroke(lineWidth: 4.0)
+                    )
+            }
+            Spacer()
+        }
+        .frame(height: 300)
+        .background(
+            ZStack {
+                Color.white//.opacity(0.2)
+                    .cornerRadius(25)
+                    .padding(20)
+                Image("\(prefix)_bg")
+                    .resizable()
+                    .padding(50)
+                    //.opacity(0.25)
+            }
+            .rotation3DEffect(.degrees(30), axis: (x: 0.0, y: -1.0, z: 0.0))
+            .opacity(0.3)
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+            .previewDevice("iPhone 13 Pro")
+            .previewInterfaceOrientation(.portrait)
+    }
+}
