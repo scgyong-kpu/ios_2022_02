08562bf874a958f001015b82a06c46f43304d05a
Kiyong Kim <scgyong@gmail.com>
2022-02-11 12:24:45 +0900

    Detail 조금 더 꾸미기

--- Day5/MapSample/MapSample/DetailView.swift
+++ Day5/MapSample/MapSample/DetailView.swift
@@ -16,28 +16,60 @@ struct DetailView: View {
         region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: poiItem.REFINE_WGS84_LAT, longitude: poiItem.REFINE_WGS84_LOGT), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     }
     var body: some View {
-        VStack {
-            Text(poiItem.SIGUN_NM)
-            Text(poiItem.SIGUN_CD)
-            Text(poiItem.REPRSNT_FOOD_NM)
-            Button {
-                let strUrl = "tel://" + poiItem.TASTFDPLC_TELNO
-                guard let url = URL(string: strUrl) else { return }
-                UIApplication.shared.open(url)
-            } label: {
-                Text(poiItem.TASTFDPLC_TELNO)
-            }
+        GeometryReader { gr in
+            ScrollView(.vertical) {
+                VStack(alignment:.leading) {
+                    VStack(alignment:.leading) {
+                        HStack {
+                            Image(systemName: "tram.circle")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text(poiItem.SIGUN_NM)
+                        }
+                        HStack {
+                            Image(systemName: "location.circle")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text(poiItem.SIGUN_CD)
+                        }
+                        HStack {
+                            Image(systemName: "fork.knife.circle")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text(poiItem.REPRSNT_FOOD_NM)
+                        }
+                        HStack {
+                            Image(systemName: "phone.circle")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Button {
+                                let strUrl = "tel://" + poiItem.TASTFDPLC_TELNO
+                                guard let url = URL(string: strUrl) else { return }
+                                UIApplication.shared.open(url)
+                            } label: {
+                                Text(poiItem.TASTFDPLC_TELNO)
+                            }
+                        }
+                    }
+                    .padding()
 
-           Map(coordinateRegion: $region)
-                .frame(maxWidth:.infinity, maxHeight:.infinity)
+                   Map(coordinateRegion: $region)
+                        .frame(
+                            width: gr.size.width,
+                            height: gr.size.height / 2
+                        )
+                }
+                .navigationTitle(poiItem.RESTRT_NM)
+            .navigationBarTitleDisplayMode(.inline)
+            }
         }
-        .navigationTitle(poiItem.RESTRT_NM)
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(poiItem: PoiData.rows[0])
+        NavigationView {
+            DetailView(poiItem: PoiData.rows[0])
+        }
     }
 }
